583b26c37afe62be5a51f8a9156bb92a3b135228
joseenrique61 <joseenrique6161@gmail.com>
2022-11-12 02:51:33 -0500

    Version 2019

---
+++ Assets/Scripts/CubeRigidBody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRigidBody : MonoBehaviour
+{
+    void OnSelect()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
